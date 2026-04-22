25360e7e817f8bf0919df04fbd43862457351896
EddieMora21 <moramartin2118@gmail.com>
2026-04-22 14:17:10 -0600

    fix: remove frontend config redeclarations

--- js/config.js
+++ js/config.js
@@ -19,9 +19,3 @@ window.API_CONFIG = {
     
     IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500'
 };
- 
-// Alias para compatibilidad global (usando var para global scope)
-var API_URL = window.API_CONFIG.API_URL;
-var AUTH_URL = window.API_CONFIG.AUTH_URL;
-var GENEROS_URL = window.API_CONFIG.GENEROS_URL;
-var IMAGE_BASE_URL = window.API_CONFIG.IMAGE_BASE_URL;
